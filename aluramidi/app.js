66950289bbde264ce86ec6716338d63cf7663a22
Antonio Alberto <antonioalbertodsb@gmail.com>
2022-08-10 11:44:57 -0400

    resolvendo o problemas das teclas

--- aluramidi/app.js
+++ aluramidi/app.js
@@ -9,4 +9,14 @@ for (let index = 0; index < listaTeclas.length; index++) {
     listaTeclas[index].onclick = () => {
         arrayAudio[index].play()
     }
+
+    listaTeclas[index].onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            listaTeclas[index].classList.add('ativa')
+        }
+    }
+
+    listaTeclas[index].onkeyup = function () {
+        listaTeclas[index].classList.remove('ativa')
+    }
 }
